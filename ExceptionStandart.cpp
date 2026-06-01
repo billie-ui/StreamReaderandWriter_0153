11c87e6ae495fa947458f99faa9b4e0321d2d2db
Billie Fauzan <kuliahkuliahbillie@gmail.com>
2026-06-01 16:26:21 +0700

    Menambahkan penanganan exception dengan catch

--- ExceptionStandart.cpp
+++ ExceptionStandart.cpp
@@ -10,5 +10,12 @@ int main()
         // throw 0.5; // melemparkan sebuah integer maka
         cout << "Pernyataan tidak akan ditampilkan" << endl;
     }
+     // kondisi jika terjadi error di dalam try
+    catch (exception& e)
+    {
+        cout << e.what() << endl;
+    }
 
+    cout << "Baris program terakhir" << endl; // penanda 2
+    return 0;
 }
